fce3ab54e6c07c81afc92163e626303884f14aa7
raily2595 <raily2595@hotmail.de>
2023-07-03 18:51:21 +0200

    starte på farge karusell

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -94,13 +94,43 @@ const MainContent = () => {
                     <div className="carousel-container">
                         <Carousel>
                             <div>
-                                <img src={`${process.env.PUBLIC_URL}nila.jpg`} alt="Nila" />
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Blue52F.jpg`} alt="Nila" />
                             </div>
                             <div>
-                                <img src={`${process.env.PUBLIC_URL}nila.jpg`} alt="Nila" />
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Blue521.jpg`} alt="Nila" />
                             </div>
                             <div>
-                                <img src={`${process.env.PUBLIC_URL}nila.jpg`} alt="Nila" />
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Brown522.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Coral521.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Gold-leaf522.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Green52K.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Green525.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Lagune52F.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Orange529.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Pink521.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Purple524.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Red522.jpg`} alt="Nila" />
+                            </div>
+                            <div>
+                                <img src={`${process.env.PUBLIC_URL}fargekart/Wine521.jpg`} alt="Nila" />
                             </div>
                         </Carousel>
                     </div>
