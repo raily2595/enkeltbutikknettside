4fdc9da8634c69f5068cfcf6547833d6e4872deb
Fredrik C. Giertsen <fcgiertsen@gmail.com>
2023-07-14 00:28:35 +0200

    button styling (#19)
    * button styling
    
    * styling of cards yet  again
    
    * remove wrong comment
    
    * flex to three on a row  if possible
    
    * fix for irregular image sizes
    
    ---------
    
    Co-authored-by: giert <“fcgiertsen@gmail.com”>

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -10,108 +10,128 @@ const MainContent = () => {
                 <div className="card">
                     <div className="carousel-container">
                         <Carousel>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/leieband1.jpg`} alt="Nila" />
-                            </div>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/leieband2.jpg`} alt="Nila" />
-                            </div>
+                            <img src={`${process.env.PUBLIC_URL}bilder/leieband1.jpg`} alt="Nila" />
+                            <img src={`${process.env.PUBLIC_URL}bilder/leieband2.jpg`} alt="Nila" />
                         </Carousel>
                     </div>
                     <div className="card-content">
-                        <h2>{strings.leieband}</h2>
-                        <p>{strings.leiebandtekst1}{strings.fargetekst}{strings.lengdetekst}</p>
-                        <p>{strings.leiebandtekst2}</p>
-                        <p>{strings.vinylinfo}</p>
+                        <div>
+                            <h2>{strings.leieband}</h2>
+                            <p>{strings.leiebandtekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.fargetekst}</p>
+                            <p>{strings.lengdetekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.prisinfoband}</p>
+                            <p>{strings.vinylinfo}</p>
+                        </div>
                         <a href="https://forms.gle/kY8AKAE7vqwSmW2u5" className="card-button" target="_blank" rel="noreferrer">{strings.bestillknapp}</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="carousel-container">
                         <Carousel>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/halsband1.jpg`} alt="Nila" />
-                            </div>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/halsband2.jpg`} alt="Nila" />
-                            </div>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/halsband3.jpg`} alt="Nila" />
-                            </div>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/halsband4.jpg`} alt="Nila" />
-                            </div>
+                            <img src={`${process.env.PUBLIC_URL}bilder/halsband1.jpg`} alt="Nila" />
+                            <img src={`${process.env.PUBLIC_URL}bilder/halsband2.jpg`} alt="Nila" />
+                            <img src={`${process.env.PUBLIC_URL}bilder/halsband3.jpg`} alt="Nila" />
+                            <img src={`${process.env.PUBLIC_URL}bilder/halsband4.jpg`} alt="Nila" />
                         </Carousel>
                     </div>
                     <div className="card-content">
-                        <h2>{strings.halsband}</h2>
-                        <p>{strings.halsbandtekst1}</p>
-                        <p>{strings.halsbandtekst2}</p>
-                        <p>{strings.vinylinfo}</p>
+                        <div>
+                            <h2>{strings.halsband}</h2>
+                            <p>{strings.halsbandtekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.fargetekst}</p>
+                            <p>{strings.lengdetekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.prisinfohalsband}</p>
+                            <p>{strings.vinylinfo}</p>
+                        </div>
                         <a href="https://forms.gle/kY8AKAE7vqwSmW2u5" className="card-button" target="_blank" rel="noreferrer">{strings.bestillknapp}</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="carousel-container">
                         <Carousel>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/sporline1.jpg`} alt="Nila" />
-                            </div>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/sporline2.jpg`} alt="Nila" />
-                            </div>
+                            <img src={`${process.env.PUBLIC_URL}bilder/sporline1.jpg`} alt="Nila" />
+                            <img src={`${process.env.PUBLIC_URL}bilder/sporline2.jpg`} alt="Nila" />
                         </Carousel>
                     </div>
                     <div className="card-content">
-                        <h2>{strings.sporline}</h2>
-                        <p>{strings.sporlinetekst1}{strings.fargetekst}{strings.lengdetekst}</p>
-                        <p>{strings.sporlinetekst2}</p>
-                        <p>{strings.vinylinfo}</p>
+                        <div>
+                            <h2>{strings.sporline}</h2>
+                            <p>{strings.sporlinetekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.fargetekst}</p>
+                            <p>{strings.lengdetekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.prisinfoband}</p>
+                            <p>{strings.vinylinfo}</p>
+                        </div>
                         <a href="https://forms.gle/kY8AKAE7vqwSmW2u5" className="card-button" target="_blank" rel="noreferrer">{strings.bestillknapp}</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="carousel-container">
                         <Carousel>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/dobbelkrok1.jpg`} alt="Nila" />
-                            </div>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/dobbelkrok2.jpg`} alt="Nila" />
-                            </div>
+                            <img src={`${process.env.PUBLIC_URL}bilder/dobbelkrok1.jpg`} alt="Nila" />
+                            <img src={`${process.env.PUBLIC_URL}bilder/dobbelkrok2.jpg`} alt="Nila" />
                         </Carousel>
                     </div>
                     <div className="card-content">
-                        <h2>{strings.dobbeltkrok}</h2>
-                        <p>{strings.dobbeltkroktekst1}{strings.fargetekst}{strings.lengdetekst}</p>
-                        <p>{strings.dobbeltkroktekst2}</p>
-                        <p>{strings.vinylinfo}</p>
+                        <div>
+                            <h2>{strings.dobbeltkrok}</h2>
+                            <p>{strings.dobbeltkroktekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.fargetekst}</p>
+                            <p>{strings.lengdetekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.prisinfoband}</p>
+                            <p>{strings.vinylinfo}</p>
+                        </div>
                         <a href="https://forms.gle/kY8AKAE7vqwSmW2u5" className="card-button" target="_blank" rel="noreferrer">{strings.bestillknapp}</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="carousel-container">
                         <Carousel>
-                            <div>
-                                <img src={`${process.env.PUBLIC_URL}bilder/hanefot1.jpg`} alt="Nila" />
-                            </div>
+                            <img src={`${process.env.PUBLIC_URL}bilder/hanefot1.jpg`} alt="Nila" />
                         </Carousel>
                     </div>
                     <div className="card-content">
-                        <h2>{strings.hanefot}</h2>
-                        <p>{strings.hanefottekst1}{strings.fargetekst}{strings.lengdetekst}</p>
-                        <p>{strings.hanefottekst2}</p>
-                        <p>{strings.vinylinfo}</p>
+                        <div>
+                            <h2>{strings.hanefot}</h2>
+                            <p>{strings.hanefottekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.fargetekst}</p>
+                            <p>{strings.lengdetekst}</p>
+                        </div>
+                        <div>
+                            <p>{strings.prisinfoband}</p>
+                            <p>{strings.vinylinfo}</p>
+                        </div>
                         <a href="https://forms.gle/kY8AKAE7vqwSmW2u5" className="card-button" target="_blank" rel="noreferrer">{strings.bestillknapp}</a>
                     </div>
                 </div>
                 <div className="card">
-                    <div>
-                        <img src={`${process.env.PUBLIC_URL}nila.jpg`} alt="Nila" />
-                    </div>
-                    <div className="card-content">
-                        <h2>{strings.info}</h2>
-                        <p>{strings.infotekst}</p>
+                    <div className="card-lone-content">
+                        <div>
+                            <h2>{strings.info}</h2>
+                            <p>{strings.infotext1}</p>
+                            <p>{strings.infotext2}</p>
+                            <p>{strings.infotext3}</p>
+                            <p>{strings.infotext4}</p>
+                        </div>
                         <NavLink
                             className="card-button"
                             activeclassname="nav-link-active"
