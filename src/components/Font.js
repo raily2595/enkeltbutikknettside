ff4e5bd7db9ad3367d6e3cfed4e3ddba12f55678
giert <fcgiertsen@gmail.com>
2023-07-14 09:30:14 +0200

    to kolonner

--- src/components/Font.js
+++ src/components/Font.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const FontPage = () => {
     const [text, setText] = useState('Navn');
@@ -12,54 +12,76 @@ const FontPage = () => {
         <div>
             <h1>Din tekst</h1>
             <input type="text" value={text} onChange={handleInputChange} />
-            <table>
-                <thead>
-                    <tr>
-                        <th>Navn</th>
-                        <th>Font</th>
-                        <th>Navn</th>
-                        <th>Font</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>Arial</th>
-                        <th><div style={{ fontFamily: 'Arial', fontSize: '50px' }}>{text}</div></th>
-                        <th>Tøff</th>
-                        <th><div style={{ fontFamily: 'Tøff', fontSize: '50px' }}>{text}</div></th>
-                    </tr>
-                    <tr>
-                        <th>Courier New</th>
-                        <th><div style={{ fontFamily: 'Courier New', fontSize: '50px' }}>{text}</div></th>
-                        <th>Calibri</th>
-                        <th><div style={{ fontFamily: 'Calibri', fontSize: '50px' }}>{text}</div></th>
-                    </tr>
-                    <tr>
-                        <th>Comic Sans</th>
-                        <th><div style={{ fontFamily: 'Comic Sans', fontSize: '50px' }}>{text}</div></th>
-                        <th>Stjernekrig</th>
-                        <th><div style={{ fontFamily: 'Stjernekrig', fontSize: '50px' }}>{text}</div></th>
-                    </tr>
-                    <tr>
-                        <th>Times New Roman</th>
-                        <th><div style={{ fontFamily: 'Times New Roman', fontSize: '50px' }}>{text}</div></th>
-                        <th>Magisk</th>
-                        <th><div style={{ fontFamily: 'Magisk', fontSize: '50px' }}>{text}</div></th>
-                    </tr>
-                    <tr>
-                        <th>Håndskrevet</th>
-                        <th><div style={{ fontFamily: 'Handskrevet', fontSize: '50px' }}>{text}</div></th>
-                        <th>Skog</th>
-                        <th><div style={{ fontFamily: 'Skog', fontSize: '50px' }}>{text}</div></th>
-                    </tr>
-                    <tr>
-                        <th>Tegnefilm</th>
-                        <th><div style={{ fontFamily: 'Tegnefilm', fontSize: '50px' }}>{text}</div></th>
-                        <th>Barnebok</th>
-                        <th><div style={{ fontFamily: 'Barnebok', fontSize: '50px' }}>{text}</div></th>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="font-container">
+                <table className="font-tabell">
+                    <thead>
+                        <tr>
+                            <th style={{ width: 180 + 'px' }}>Navn</th>
+                            <th style={{ width: 360 + 'px' }}>Font</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Tøff</th>
+                            <th><div style={{ fontFamily: 'Tøff', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Calibri</th>
+                            <th><div style={{ fontFamily: 'Calibri', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Stjernekrig</th>
+                            <th><div style={{ fontFamily: 'Stjernekrig', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Magisk</th>
+                            <th><div style={{ fontFamily: 'Magisk', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Skog</th>
+                            <th><div style={{ fontFamily: 'Skog', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Barnebok</th>
+                            <th><div style={{ fontFamily: 'Barnebok', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className="font-tabell">
+                    <thead>
+                        <tr>
+                            <th style={{ width: 180 + 'px' }}>Navn</th>
+                            <th style={{ width: 360 + 'px' }}>Font</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Arial</th>
+                            <th><div style={{ fontFamily: 'Arial', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Courier New</th>
+                            <th><div style={{ fontFamily: 'Courier New', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Comic Sans</th>
+                            <th><div style={{ fontFamily: 'Comic Sans', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Times New Roman</th>
+                            <th><div style={{ fontFamily: 'Times New Roman', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Håndskrevet</th>
+                            <th><div style={{ fontFamily: 'Handskrevet', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                        <tr>
+                            <th>Tegnefilm</th>
+                            <th><div style={{ fontFamily: 'Tegnefilm', fontSize: '50px' }}>{text}</div></th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <div>
                 <NavLink
                     className="nav-link"
@@ -67,7 +89,7 @@ const FontPage = () => {
                     exact="true"
                     to="/">Startside</NavLink>
             </div>
-        </div>
+        </div >
     );
 };
 
