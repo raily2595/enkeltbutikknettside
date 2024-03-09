a89adde32fb506c1edea1784a2bf070a830d0202
giert <fcgiertsen@gmail.com>
2024-03-09 22:26:46 +0100

    missing import

--- src/components/Bestilling/Handlekurv.js
+++ src/components/Bestilling/Handlekurv.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const Handlekurv = () => {
     const [handlekurvItems, setHandlekurvItems] = useState([]);
